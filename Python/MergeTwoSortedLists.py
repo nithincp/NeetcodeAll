d35bb26b6771e9cc848440241a5c914c602c15fa
Nithin Chandran P <p.nithinc@gmail.com>
2023-01-12 15:22:14 +0530

    Merge Two Sorted List py version

---
+++ Python/MergeTwoSortedLists.py
@@ -0,0 +1,25 @@
+class ListNode:
+
+    def __init__(self,val,next) -> None:
+        self.val = val
+        self.next = next
+    
+
+def mergeTwoSortedLists(list1,list2):
+    cur = dummy = ListNode()
+
+    while list1 and list2:
+        if list2.val < list1.val:
+            cur.next = list2
+            list2 = list2.next
+        else:
+            cur.next = list1
+            list1 = list1.next
+        cur = cur.next
+
+    if list1:
+        cur.next = list1
+    elif list2:
+        cur.next = list2
+
+    return dummy.next
