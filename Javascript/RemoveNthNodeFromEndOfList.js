836c4f4fa23b2770110769094d4c467b0934d082
Nithin Chandran P <p.nithinc@gmail.com>
2023-02-01 12:10:52 +0530

    Remove Nth node from end of list js version

---
+++ Javascript/RemoveNthNodeFromEndOfList.js
@@ -0,0 +1,24 @@
+function ListNode(val,next){
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
+
+
+var removeNthNode = function(head,n){
+    let dummy = new ListNode(0,head)
+    let left = dummy
+    let right = head
+
+    while(n>0 && right){
+        right = right.next
+        n-=1
+    }
+
+    while(right){
+        left = left.next
+        right = right.next
+    }
+
+    left.next = left.next.next
+    return dummy.next
+}
