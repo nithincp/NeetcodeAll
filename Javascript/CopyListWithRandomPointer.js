6bab88f701afc5ea9061cb6510de69b5cd9f990f
Nithin Chandran P <p.nithinc@gmail.com>
2023-02-06 15:26:37 +0530

    Copy List With Random Pointer js version

---
+++ Javascript/CopyListWithRandomPointer.js
@@ -0,0 +1,28 @@
+function Node(val,next,random){
+    this.val = val
+    this.next = next
+    this.random = random
+}
+
+var copyList = function(head){
+    let oldToCopy = new Map()
+    oldToCopy.set(null,null)
+    let cur = head
+
+    while(cur){
+        let copy = new Node(cur.val)
+        oldToCopy.set(cur,copy)
+        cur = cur.next
+    }
+
+    cur = head
+    while(cur){
+        let copy = oldToCopy.get(cur)
+        copy.next = oldToCopy.get(cur.next)
+        copy.random = oldToCopy.get(cur.random)
+        cur = cur.next
+    }
+
+    return oldToCopy.get(head)
+
+}
