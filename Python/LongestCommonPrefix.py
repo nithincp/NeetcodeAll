9fb442decd01393fc06d23e742f6968eab11e766
Nithin Chandran P <p.nithinc@gmail.com>
2023-02-22 15:06:48 +0530

    Longest Common Prefix py version

---
+++ Python/LongestCommonPrefix.py
@@ -0,0 +1,11 @@
+def longestCommonPrefix(strs):
+    prefix = ""
+
+    for i in range(len(strs[0])):
+        for s in strs:
+            if i == len(s) or s[i]!=strs[0][i]:
+                return prefix
+
+        prefix+=strs[0][i]
+
+    return prefix
