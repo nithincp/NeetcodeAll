3d3978276b9be00ec7cf1bbc702afb31f7a532cc
Nithin Chandran P <p.nithinc@gmail.com>
2023-04-20 18:20:18 +0530

    House Robber js version

---
+++ Javascript/HouseRobber.js
@@ -0,0 +1,15 @@
+var houseRobber = function(nums){
+    if (nums.length === 1){
+        return nums[0]
+    }
+
+    const dp = new Array(nums.length).fill(0)
+    dp[0] = nums[0]
+    dp[1] = Math.max(nums[0],nums[1])
+
+    for (let i=2;i<nums.length;i++){
+        dp[i] = Math.max(dp[i-1],dp[i-2]+nums[i])
+    }
+
+    return dp[dp.length-1]
+}
