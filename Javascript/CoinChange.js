774d888d5f02a5ed75db6bc5af7fde1881f71a48
Nithin Chandran P <p.nithinc@gmail.com>
2023-04-24 18:08:53 +0530

    Updated the initial value for a var

--- Javascript/CoinChange.js
+++ Javascript/CoinChange.js
@@ -2,7 +2,7 @@ var coinChange = function(coins,amount){
     const dp = new Array(amount+1).fill(amount+1)
     dp[0] = 0
 
-    for (let a=0;a<amount+1;a++){
+    for (let a=1;a<amount+1;a++){
         for(const c of coins){
             if (a-c>=0){
                 dp[a] = Math.min(dp[a],1+dp[a-c])
