1c627b7c2fbc4c6c1d78b35da60ecca31e3519dd
Nithin Chandran P <p.nithinc@gmail.com>
2023-06-07 22:09:30 +0530

    Search in a BST js

---
+++ Javascript/SearchInABinarySearchTree.js
@@ -0,0 +1,19 @@
+function TreeNode(val,right,left){
+    this.val = val
+    this.left = left
+    this.right = right
+}
+
+function searchBST(root,val){
+    if (root === null){
+        return null
+    }
+
+    if (root.val === val){
+        return root
+    }else if(val < root.val){
+        return searchBST(root.left,val)
+    }else{
+        return searchBST(root.right,val)
+    }
+}
