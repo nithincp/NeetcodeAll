aff139fbea63243a7593e01c23c2134336313569
Nithin Chandran P <p.nithinc@gmail.com>
2023-06-13 00:19:40 +0530

    Find index of first occurence js

---
+++ Javascript/FindIndexOfFirstOccurence.js
@@ -0,0 +1,18 @@
+var strStr(haystack,needle){
+    if (haystack.length < needle.length){
+        return -1
+    }
+
+    if (haystack === needle){
+        return 0
+    }
+
+    for (let i=0;i<haystack.length-needle.length+1;i++){
+        let subString = haystack.slice(i,i+needle.length)
+        if (subString === needle){
+            return i
+        }
+    }
+
+    return -1
+}
