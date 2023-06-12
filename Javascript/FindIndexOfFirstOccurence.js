e22fda903d388af9776e3cdbd977ba81e88664b5
Nithin Chandran P <p.nithinc@gmail.com>
2023-06-13 00:21:19 +0530

    missing function keyword added

--- Javascript/FindIndexOfFirstOccurence.js
+++ Javascript/FindIndexOfFirstOccurence.js
@@ -1,18 +1,18 @@
-var strStr(haystack,needle){
-    if (haystack.length < needle.length){
-        return -1
-    }
+var strStr = function (haystack, needle) {
+  if (haystack.length < needle.length) {
+    return -1;
+  }
 
-    if (haystack === needle){
-        return 0
-    }
+  if (haystack === needle) {
+    return 0;
+  }
 
-    for (let i=0;i<haystack.length-needle.length+1;i++){
-        let subString = haystack.slice(i,i+needle.length)
-        if (subString === needle){
-            return i
-        }
+  for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+    let subString = haystack.slice(i, i + needle.length);
+    if (subString === needle) {
+      return i;
     }
+  }
 
-    return -1
-}
+  return -1;
+};
