0ed3924c8f1bec2255736ffe2a3be1aacf2c721c
Nithin Chandran P <p.nithinc@gmail.com>
2023-06-29 00:02:46 +0530

    Sqrtx binary search optimization py

--- Python/Sqrtx.py
+++ Python/Sqrtx.py
@@ -2,14 +2,33 @@ import math
 
 def mySqrt(x):
 
-    if (x == 1 or x == 0):
-            return x
+    # if (x == 1 or x == 0):
+    #         return x
 
-    if x == 2 or x ==3 :
-        return 1
+    # if x == 2 or x ==3 :
+    #     return 1
 
-    for i in range(0,math.ceil(x/2)+1):
-        if i*i == x:
-            return i
-        elif i*i>x:
-            return i-1
+    # for i in range(0,math.ceil(x/2)+1):
+    #     if i*i == x:
+    #         return i
+    #     elif i*i>x:
+    #         return i-1
+
+    #Using Binary Search for more optimization
+    largestValue = 0
+
+    start = 0
+    end = x
+
+    while start <=end:
+        mid = (start+end)//2
+        product = mid*mid
+        if product == x:
+            return mid
+        elif product < x:
+            largestValue = mid
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return largestValue
